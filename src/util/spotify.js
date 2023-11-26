282daf12b75abf27610cfb35c625772ac902add1
Lucas Gomes <lggomes007@gmail.com>
2023-11-26 11:22:09 -0300

    criando usuario depois de redirecionar a pag

--- src/util/spotify.js
+++ src/util/spotify.js
@@ -1,44 +1,58 @@
-function loginWithSpotify () {
-    // Fetch the sensitive data from the server
-    fetch("/getSecretData")
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            const clientId = data.client_id;
-            const redirectUri = data.redirect_uri;
-            const scope = "user-read-private user-read-email";
-            console.log("Fazendo login no spotify...");
-            const authorizeUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${encodeURIComponent(scope)}&response_type=token`;
-            window.location.href = authorizeUrl;
+const User = require("../db/model/Usuario");
 
-        })
-        .catch(error => console.error("Error fetching secret data:", error));
-}
-
-function handleRedirect () {
-    const hash = window.location.hash.substring(1);
-    const params = new URLSearchParams(hash);
-
-    if (params.has("access_token")) {
-        const accessToken = params.get("access_token");
-        getUserInfo(accessToken);
-        console.log(accessToken);
+exports.loginWithSpotify = async function () {
+    try {
+        // Fetch the sensitive data from the server
+        const response = await fetch("/getSecretData");
+        const data = await response.json();
+        
+        console.log(data);
+        const clientId = data.client_id;
+        const redirectUri = data.redirect_uri;
+        const scope = "user-read-private user-read-email";
+        console.log("Fazendo login no spotify...");
+        const authorizeUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${encodeURIComponent(scope)}&response_type=token`;
+        window.location.href = authorizeUrl;
+    } catch (error) {
+        console.error("Error fetching secret data:", error);
     }
 }
 
-function getUserInfo (accessToken) {
-    fetch("https://api.spotify.com/v1/me", {
-        headers: {
-            "Authorization": `Bearer ${accessToken}`
+async function handleRedirect () {
+    try {
+        const hash = window.location.hash.substring(1);
+        const params = new URLSearchParams(hash);
+
+        if (params.has("access_token")) {
+            const accessToken = params.get("access_token");
+            
+            const userInfo = await getUserInfo(accessToken);
+            await User.create({
+                display_name: userInfo.display_name,
+                email: userInfo.email,
+                country: userInfo.country,
+                access_token: accessToken
+            });
         }
-    })
-        .then(response => response.json())
-        .then(data => {
-            const userInfoElement = document.getElementById("user-info");
-            userInfoElement.innerHTML = `<p>Bem Vindo(a), ${data.display_name}!</p>`;
-        })
-        .catch(error => console.error("Error fetching user info:", error));
+    } catch (error) {
+        console.error("Error in redirect handling:", error);
+    }
+}
+
+async function getUserInfo (accessToken) {
+    try {
+        const response = await fetch("https://api.spotify.com/v1/me", {
+            headers: {
+                "Authorization": `Bearer ${accessToken}`
+            }
+        });
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error fetching user info:", error);
+        throw error;
+    }
 }
 
 // Chamado quando a página é redirecionada de volta após o login
-handleRedirect();
+await handleRedirect();
